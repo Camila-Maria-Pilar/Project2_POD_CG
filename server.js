b8140f85afde4dc6449c9dfea916f3256a137873
Camila Galleguillos <camila.m.galleguillos@gmail.com>
2023-07-11 21:36:52 +0800

    new server line organisation - heroku

--- server.js
+++ server.js
@@ -10,22 +10,10 @@ const bcrypt = require('bcrypt');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-
-
-
-const hbs = exphbs.create({ helpers, extname: 'hbs' });
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
-
+const hbs = exphbs.create({ helpers, extname: 'hbs' });
 
 
 const sess = {
@@ -43,8 +31,15 @@ const sess = {
   })
 };
 
-
 app.use(session(sess));
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(webRoutes);
 
